ac3aab5f884c0b5baf004ffb47ce0d769f9ec2b9
suvadip rc <suvadip.roychowdhury@ehi.com>
2018-10-30 13:12:23 +0530

    Dialogflow api implemented

--- demo-aws/src/main/java/com/example/demoaws/controllers/DialogFlowController.java
+++ demo-aws/src/main/java/com/example/demoaws/controllers/DialogFlowController.java
@@ -1,27 +1,23 @@
 package com.example.demoaws.controllers;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demoaws.dtos.DialogFlowRQ;
-import com.example.demoaws.dtos.DialogFlowRS;
-import com.example.demoaws.dtos.Message;
-import com.example.demoaws.dtos.OutputContexts;
-import com.example.demoaws.dtos.TextsRS;
+import com.google.cloud.dialogflow.v2.WebhookRequest;
+import com.google.cloud.dialogflow.v2.WebhookResponse;
 
 @RestController
 public class DialogFlowController {
 
 	@PostMapping("hi")
-	public DialogFlowRS hello(@RequestBody final DialogFlowRQ dialogFlowRQ) {
+	public WebhookResponse hello(@RequestBody final WebhookRequest dialogFlowRQ) {
 		System.out.println("Inside DialogflowController.hi()");
 		System.out.println(dialogFlowRQ);
+		final WebhookResponse response = WebhookResponse.newBuilder().setFulfillmentText("Hey Suvadip RC! Welcome!!").build();
+		return response;
+		
+		/*
 		final DialogFlowRS dialogFlowRS = new DialogFlowRS();
 		dialogFlowRS.setFulfillmentText("Hello Suvadip RC! Nice to see you!! Please tell me your age.");
 		final OutputContexts contexts = new OutputContexts();
@@ -42,7 +38,7 @@ public class DialogFlowController {
 		final List<Message> messages = new ArrayList<>();
 		messages.add(message);
 		dialogFlowRS.setFulfillmentMessages(messages);
-		return dialogFlowRS;
+		return dialogFlowRS;*/
 	}
 
 }
